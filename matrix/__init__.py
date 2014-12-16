fed0a4dfe2af072c02cf283a7dbc7d9cdaa09972
jinxingxing <jxinging@gmail.com>
2014-12-16 22:36:59 +0800

    尝试实现 views 自动加载

--- matrix/__init__.py
+++ matrix/__init__.py
@@ -4,5 +4,5 @@ __version__ = '0.0.1'
 
 app = Flask(__name__)
 
-import matrix.views
+import views
 
--- matrix/views/__init__.py
+++ matrix/views/__init__.py
@@ -1,4 +1,15 @@
+# coding:utf8
+# 尝试实现 views 自动加载
+# 运行正常但，无法正常访问，访问任何地址都是一直 loading，可能是循环引用的问题？？
+# import os
+# import importlib
+#
+# cwd = os.getcwd()
+# for filename in filter(lambda s: s.endswith(".py") and not s.startswith("_"), os.listdir(cwd)):
+#     modname = os.path.splitext(filename)[0]
+#     importlib.import_module(modname)
+
+import area
 import hello
 import index
-import area
 import user
