50aa5116d33d58f9cd579c5db7cfde8016a9a771
jinxingxing <jxinging@gmail.com>
2014-12-16 22:21:18 +0800

    Nothing special

--- matrix/__init__.py
+++ matrix/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+
+__version__ = '0.0.1'
+
 app = Flask(__name__)
 
 import matrix.views
+
--- runserver.py
+++ runserver.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python2
 from matrix import app
+import sys
 
+if len(sys.argv) < 2:
+    app.run(debug=True, host='0.0.0.0', port=8080)
+    sys.exit(1)
 
-app.run(debug=True, host='0.0.0.0', port=8080)
+command = sys.argv[1]
+if command == "init-db":
+    from matrix import models
+    models.db.create_all()
+
 
